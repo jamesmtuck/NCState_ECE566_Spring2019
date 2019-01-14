edb8b229a97e95abaa8906327c4dd0bd5c2ed7d9
James Tuck <jtuck@ncsu.edu>
2019-01-14 13:08:25 -0500

    Fix to testing script

--- travis-ci-test.sh
+++ travis-ci-test.sh
@@ -2,5 +2,7 @@ set -e
 clang --version
 make -C LectureCode all
 make -C simple all
+pushd .
 cd projects && mkdir build && cd build && cmake -DLLVM_DIR=/usr/local/lib/cmake/llvm .. && cmake --build .
+popd
 make -C Tutorials/1 clean all
