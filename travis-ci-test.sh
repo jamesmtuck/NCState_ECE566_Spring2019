4e472606fb95ee522b6c31f0e97eee05ba120b09
James Tuck <jtuck@ncsu.edu>
2019-01-14 12:59:07 -0500

    enable testing of Tutorial 1

--- travis-ci-test.sh
+++ travis-ci-test.sh
@@ -3,3 +3,4 @@ clang --version
 make -C LectureCode all
 make -C simple all
 cd projects && mkdir build && cd build && cmake -DLLVM_DIR=/usr/local/lib/cmake/llvm .. && cmake --build .
+make -C Tutorials/1 clean all
