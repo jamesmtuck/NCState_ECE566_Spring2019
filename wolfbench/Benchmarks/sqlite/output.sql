285657b00a6444cd7718ea87fc28d155ae9ac0ee
James Tuck <jtuck@ncsu.edu>
2019-04-16 21:40:27 -0400

    support for tesing p4

---
+++ wolfbench/Benchmarks/sqlite/output.sql
@@ -0,0 +1,34 @@
+Table Friends created successfully
+Id = 1
+Name = Audi
+Price = 52642
+
+Id = 2
+Name = Mercedes
+Price = 57127
+
+Id = 3
+Name = Skoda
+Price = 9000
+
+Id = 4
+Name = Volvo
+Price = 29000
+
+Id = 5
+Name = Bentley
+Price = 350000
+
+Id = 6
+Name = Citroen
+Price = 21000
+
+Id = 7
+Name = Hummer
+Price = 41400
+
+Id = 8
+Name = Volkswagen
+Price = 21600
+
+exit 0
